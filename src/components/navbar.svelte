dfdbf4345e0a24e777cefda893c72467465930b3
alancj731 <alanchenjian@gmail.com>
2024-07-28 17:31:26 -0500

    try font awesome iconn

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -62,7 +62,7 @@
 	<div
 		class="w-full h-[100px] px-4 py-3 m-1 rounded-sm border border-[#e4e4e7] border-1[px] bg-darkgray text-lightgreen font-bold sm:min-h-[330px] mt-3 ml-3 sm:w-[250px]"
 	>
-		<i class="fas fa-bars">operation</i>
+		<i class="fas fa-bars">Try</i>
 
 		<nav class="ml-2 mt-2 text-xs">
 			<ul class="flex flex-col space-y-2 font-normal text-[14px]">
